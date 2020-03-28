14775b86ee0585b2f40843d215e1f3adf6f101ad
Rajdeep Vijayakar <rajdeepvijayakar@rajdeeps-air.mynet>
2020-03-28 20:34:37 +0000

    corrected the users fetch url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https:/jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then(res => res.json())
       .then(users => this.setState({ monsters: users }));
   }
