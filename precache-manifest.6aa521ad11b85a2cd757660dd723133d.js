b034a2271596b818a51ad79a3c9f9545182dfe5b
Rajdeep Vijayakar <rajdeepvijayakar@rajdeeps-air.mynet>
2020-03-28 20:30:56 +0000

    Updates

---
+++ precache-manifest.6aa521ad11b85a2cd757660dd723133d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4674d7a337a1f11b3bb26db0c55e8ca",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "f607213087ec886dd136",
+    "url": "/react-monsters/static/css/main.1e9c77af.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f607213087ec886dd136",
+    "url": "/react-monsters/static/js/main.42a270d4.chunk.js"
+  },
+  {
+    "revision": "9e48f53adff59196fe9e",
+    "url": "/react-monsters/static/js/runtime-main.265b368b.js"
+  }
+]);
